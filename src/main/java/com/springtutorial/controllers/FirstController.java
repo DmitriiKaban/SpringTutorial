4c80bb6c594a29fbd34203ab6ff466617ea28f97
Dmitrii Cravcenco <dima16kravchenko@gmail.com>
2023-08-15 11:40:40 +0300

    First way to acquire data from get request

--- src/main/java/com/springtutorial/controllers/FirstController.java
+++ src/main/java/com/springtutorial/controllers/FirstController.java
@@ -3,11 +3,19 @@ package com.springtutorial.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 public class FirstController {
 
     @GetMapping("hello")
-    public String helloPage(){
+    public String helloPage(HttpServletRequest request){
+        // reads /hello?name=Tom&surname=Jones
+        String name = request.getParameter("name");
+        String surname = request.getParameter("surname");
+
+        System.out.println(name + " " + surname);
+
         return "first/hello";
     }
     @GetMapping("goodbye")
