19688f19dd6c1f404e8f4cdc1d9d8472921e76a6
Dmitrii Cravcenco <dima16kravchenko@gmail.com>
2023-08-15 12:44:35 +0300

    Added the possibility to edit and delete a person

--- src/main/java/com/springtutorial/config/MySpringMVCDispatcherServletInitializer.java
+++ src/main/java/com/springtutorial/config/MySpringMVCDispatcherServletInitializer.java
@@ -1,6 +1,10 @@
 package com.springtutorial.config;
 
+import org.springframework.web.filter.HiddenHttpMethodFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
 
 public class MySpringMVCDispatcherServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
     protected Class<?>[] getRootConfigClasses() {
@@ -15,4 +19,17 @@ public class MySpringMVCDispatcherServletInitializer extends AbstractAnnotationC
     protected String[] getServletMappings() {
         return new String[] {"/"} ;
     }
+
+    @Override
+    public void onStartup(ServletContext aServletContext) throws ServletException {
+        super.onStartup(aServletContext);
+        registerHiddenFieldFilter(aServletContext);
+    }
+
+    // allows to work PATCH and DELETE request, because it reads hidden fields in the request
+    private void registerHiddenFieldFilter(ServletContext aContext) {
+        aContext.addFilter("hiddenHttpMethodFilter",
+                new HiddenHttpMethodFilter()).addMappingForUrlPatterns(null ,true, "/*");
+    }
+
 }
