afc9f32d10c010959743d51a895d454b2c1036db
Dmitrii Cravcenco <dima16kravchenko@gmail.com>
2023-08-15 11:29:07 +0300

    Moved config files from .xml files to .java files and added a dependency for .java config file

---
+++ src/main/java/com/springtutorial/config/MySpringMVCDispatcherServletInitializer.java
@@ -0,0 +1,18 @@
+package com.springtutorial.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class MySpringMVCDispatcherServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    protected Class<?>[] getRootConfigClasses() {
+        return null;
+    }
+
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[] {SpringConfig.class};
+    }
+
+    // all the request should be directed to dispatcher servlet
+    protected String[] getServletMappings() {
+        return new String[] {"/"} ;
+    }
+}
