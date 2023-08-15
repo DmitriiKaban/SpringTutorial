6e85109e07c46a5477371699a39d9470790edc05
Dmitrii Cravcenco <dima16kravchenko@gmail.com>
2023-08-15 14:37:29 +0300

    Added validation to all the forms + fields for Person object

--- src/main/java/com/springtutorial/controllers/PeopleController.java
+++ src/main/java/com/springtutorial/controllers/PeopleController.java
@@ -5,7 +5,10 @@ import com.springtutorial.models.Person;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/people")
@@ -41,10 +44,12 @@ public class PeopleController {
     }
 
     @PostMapping()
-    public String create(@ModelAttribute("person") Person person) {
+    public String create(@ModelAttribute("person") @Valid Person person, BindingResult bindingResult) {
+
+        if (bindingResult.hasErrors())
+            return "people/new"; // return
 
         personDAO.save(person);
-
         return "redirect:/people";
     }
 
@@ -57,9 +62,12 @@ public class PeopleController {
     }
 
     @PatchMapping("/{id}")
-    public String update(@ModelAttribute("person") Person person, @PathVariable("id") int id) {
-        personDAO.update(id, person);
+    public String update(@ModelAttribute("person") @Valid Person person, BindingResult bindingResult, @PathVariable("id") int id) {
 
+        if (bindingResult.hasErrors())
+            return "people/edit";
+
+        personDAO.update(id, person);
         return "redirect:/people";
     }
 
