5e64cba47bdbbf6860c8485cf294e00dbef379a5
Dmitrii Cravcenco <dima16kravchenko@gmail.com>
2023-08-15 22:58:20 +0300

    Changed custom rowmapper to default

--- src/main/java/com/springtutorial/dao/PersonDAO.java
+++ src/main/java/com/springtutorial/dao/PersonDAO.java
@@ -3,6 +3,7 @@ package com.springtutorial.dao;
 
 import com.springtutorial.models.Person;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 import java.sql.*;
@@ -20,11 +21,11 @@ public class PersonDAO {
 
 
     public List<Person> index() {
-        return jdbcTemplate.query("SELECT * FROM Person", new PersonMapper());
+        return jdbcTemplate.query("SELECT * FROM Person", new BeanPropertyRowMapper<>(Person.class));
     }
 
     public Person show(int id) {
-        return jdbcTemplate.query("SELECT * FROM Person WHERE id=?", new Object[]{id}, new PersonMapper()).stream().findAny().orElse(null);
+        return jdbcTemplate.query("SELECT * FROM Person WHERE id=?", new Object[]{id}, new BeanPropertyRowMapper<>(Person.class)).stream().findAny().orElse(null);
     }
 
     public void save(Person person) {
