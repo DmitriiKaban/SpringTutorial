9b4a99abf97cd4577fb69a18c51196fbdaa44068
Dmitrii Cravcenco <dima16kravchenko@gmail.com>
2023-08-19 10:30:16 +0300

    Added Pattern Validation for a new field in the class Person

--- src/main/java/com/springtutorial/models/Person.java
+++ src/main/java/com/springtutorial/models/Person.java
@@ -1,13 +1,7 @@
 package com.springtutorial.models;
 
-import javax.validation.constraints.Email;
-import javax.validation.constraints.Min;
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.Size;
+import javax.validation.constraints.*;
 
-/**
- * @author Neil Alishev
- */
 public class Person {
     private int id;
 
@@ -22,15 +16,22 @@ public class Person {
     @Email(message = "Email should be valid")
     private String email;
 
+    // Country, City, Index (6 nums)
+    // Moldova, Chisinau, 123456
+    @Pattern(regexp = "[A-Z]\\w+, [A-Z]\\w+, \\d{6}", message = "Your address should be in this form: Country, City, Postal Code(6 digits)")
+    private String address;
+
     public Person() {
 
     }
 
-    public Person(int id, String name, int age, String email) {
+
+    public Person(int id, String name, int age, String email, String address) {
         this.id = id;
         this.name = name;
         this.age = age;
         this.email = email;
+        this.address = address;
     }
 
     public int getId() {
@@ -63,5 +64,13 @@ public class Person {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
     }
 }
